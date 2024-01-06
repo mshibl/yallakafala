7af6b23b7dbff450f07e1c0100a4c49376f752a2
mohamedar97 <mohamedraafat060@gmail.com>
2024-01-06 21:58:37 +0200

    Refactoring and fine-tuning the footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
-import { Typography } from "@mui/material";
 import AppFooter from "../components/footer/index";
 
 const Home = () => {
   return (
     <>
-      <Typography variant="body1">Homepage</Typography>
       <AppFooter />
     </>
   );
