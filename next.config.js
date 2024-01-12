716cf8d8b2d3d310f767d215bda83255d4e9eafe
Mo <mohamed@way2b1.com>
2024-01-11 21:38:03 -0800

    allow loading iframe route

--- next.config.js
+++ next.config.js
@@ -1,4 +1,33 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors '*';
+    block-all-mixed-content;
+    upgrade-insecure-requests;
+`
 
-module.exports = nextConfig
+
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/iframe/*",
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
+          },
+        ],
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
