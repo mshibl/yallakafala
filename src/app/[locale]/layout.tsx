cf5dd135a120ab1c63666bfd56aaa16c31405743
mohamedar97 <mohamedraafat060@gmail.com>
2024-01-13 19:54:31 +0200

    Added i18n configuration

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,20 +1,26 @@
 import ThemeProvider from "@mui/material/styles/ThemeProvider";
 import type { Metadata } from "next";
-import AppTheme from "../utils/AppTheme";
-import "../utils/global.css";
+import AppTheme from "@/src/utils/AppTheme";
+import "@/src/utils/global.css";
 
 export const metadata: Metadata = {
   title: "Yalla Kafala",
   description: "Yalla Kafala",
 };
 
-const RootLayout = ({ children }: { children: React.ReactNode }) => {
+const LocaleLayout = ({
+  children,
+  params: { locale },
+}: {
+  children: React.ReactNode;
+  params: { locale: any };
+}) => {
   return (
-    <html lang="en">
+    <html dir={locale === "en" ? "ltr" : "rtl"} lang={locale}>
       <ThemeProvider theme={AppTheme}>
         <body>{children}</body>
       </ThemeProvider>
     </html>
   );
 };
-export default RootLayout;
+export default LocaleLayout;
