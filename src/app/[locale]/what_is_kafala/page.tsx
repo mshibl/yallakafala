a3d06f01885a6d2d898a2e7864d1991239ad6897
mohamedar97 <mohamedraafat060@gmail.com>
2024-02-13 03:36:24 +0200

    Added an image banner at the top of the page

--- src/app/[locale]/what_is_kafala/page.tsx
+++ src/app/[locale]/what_is_kafala/page.tsx
@@ -1,27 +1,31 @@
 import React from "react";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import IntroParagraphs from "./introParagraphs";
 import FirstHadith from "./firstHadith";
 import SecondHadith from "./secondHadith";
 import Footnotes from "./footnotes";
 import References from "./references";
+import Banner from "./banner";
 
 const WhatIsKafalaPage = () => {
   return (
-    <Box
-      sx={{
-        maxWidth: "940px",
-        margin: "auto",
-        paddingY: "60px",
-        paddingX: "20px",
-      }}
-    >
-      <IntroParagraphs />
-      <FirstHadith />
-      <SecondHadith />
-      <Footnotes />
-      <References />
-    </Box>
+    <>
+      <Banner />
+      <Box
+        sx={{
+          maxWidth: "940px",
+          margin: "auto",
+          paddingY: { xs: "40px", md: "60px" },
+          paddingX: "20px",
+        }}
+      >
+        <IntroParagraphs />
+        <FirstHadith />
+        <SecondHadith />
+        <Footnotes />
+        <References />
+      </Box>
+    </>
   );
 };
 
