f65e198ed40025d948c7418ad2d4fdb68ea2e665
Mo <mohamed@way2b1.com>
2024-03-15 22:34:15 -0700

    adds geolocation handling

--- src/components/AppFooter/CopyrightStatement.tsx
+++ src/components/AppFooter/CopyrightStatement.tsx
@@ -1,3 +1,5 @@
+import { getLocationDataFromCookies } from "@/src/utils/geolocation";
+import { Box } from "@mui/material";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import { useLocale, useTranslations } from "next-intl";
@@ -5,29 +7,54 @@ import { headers } from "next/headers";
 
 const CopyrightStatement = () => {
   const locale = useLocale();
-  const country = headers().get("country");
-  let text = "";
+  const locationData = getLocationDataFromCookies();
+  const country = locationData.country.toLowerCase();
+
+  let text: JSX.Element | string = "";
 
   const currentYear = new Date().getFullYear();
 
-  if (country === "US") {
-    text =
-      locale === "ar"
-        ? `جميع الحقوق محفوظة © ${currentYear}. جميع الحقوق محفوظة | يلا كفالة هي منظمة غير ربحية مسجلة 501c3. جميع التبرعات معفاة من الضرائب. EIN: 85-3323627`
-        : `Copyright © ${currentYear}. All rights reserved | Yalla Kafala USA is a registered 501c3 nonprofit organization. All donations are tax deductible. EIN: 85-3323627`;
+  if (country === "us") {
+    if (locale === "ar") {
+      text = (
+        <>
+          يلا كفالة هي منظمة غير ربحية مسجلة 501(c)3. جميع التبرعات معفاة من
+          الضرائب EIN: 85-3323627
+          <br />© {currentYear} جميع الحقوق محفوظة
+        </>
+      );
+    } else {
+      text = (
+        <>
+          Yalla Kafala is a registered 501(c)3 nonprofit organization. All
+          donations are tax deductible. EIN: 85-3323627
+          <br />© {currentYear} All rights reserved
+        </>
+      );
+    }
   } else {
-    text =
-      locale === "ar"
-        ? `© جميع الحقوق محفوظة ${currentYear} ، تأسست  يلا كفالة فى مصر في عام ٢٠٢١ برقم إشهار ( ٩٩٠)`
-        : `© All rights reserved ${currentYear}, Yalla Kafala was established in Egypt in 2021 with a license number (990)`;
+    if (locale === "ar") {
+      text = (
+        <>
+          تأسست يلا كفالة في مصر في عام 2021 برقم إشهار (990)
+          <br />© جميع الحقوق محفوظة {currentYear}
+        </>
+      );
+    } else {
+      text = (
+        <>
+          Yalla Kafala was established in Egypt in 2021 with a license number
+          (990)
+          <br />© {currentYear} All rights reserved
+        </>
+      );
+    }
   }
 
   return (
     <Grid item container md={12} xs={12} mt={"20px"} pb="50px">
       <Grid item xs={12}>
-        <Typography variant="subtitle1" fontWeight={600}>
-          {text}
-        </Typography>
+        <Typography variant="subtitle1">{text}</Typography>
       </Grid>
     </Grid>
   );
--- src/components/AppFooter/index.tsx
+++ src/components/AppFooter/index.tsx
@@ -4,43 +4,35 @@ import ContactList from "./contact/ContactList";
 import CopyrightStatement from "./CopyrightStatement";
 import SocialMediaList from "./newsletter/SocialMediaList";
 import SilverBadge from "./newsletter/SilverBadge";
-import { cookies } from "next/headers";
-import { Box } from "@mui/material";
 
 const AppFooter: React.FC = () => {
-  const locationData = cookies().get("locationData");
-  console.log(locationData?.value);
-
   return (
-    <>
-      {locationData && <Box display="">{locationData?.value}</Box>}
+    <Grid
+      container
+      sx={{
+        bgcolor: "primary.main",
+        color: "#ffffff",
+        px: { xs: 20, md: 45 },
+        pt: { xs: 17, md: 34 },
+        pb: { xs: 10, md: 15 },
+      }}
+    >
+      <QuickLinksList />
+      <ContactList />
       <Grid
         container
-        sx={{
-          bgcolor: "primary.main",
-          color: "#ffffff",
-          px: { xs: 20, md: 45 },
-          pt: { xs: 17, md: 34 },
-          pb: { xs: 10, md: 15 },
-        }}
+        item
+        sx={{ mb: { xs: 30, md: 0 } }}
+        direction={"column"}
+        xs={12}
+        md={4}
       >
-        <QuickLinksList />
-        <ContactList />
-        <Grid
-          container
-          item
-          sx={{ mb: { xs: 30, md: 0 } }}
-          direction={"column"}
-          xs={12}
-          md={4}
-        >
-          {/* <NewsletterList /> */}
-          <SocialMediaList />
-          <SilverBadge />
-        </Grid>
-        <CopyrightStatement />
+        {/* <NewsletterList /> */}
+        <SocialMediaList />
+        <SilverBadge />
       </Grid>
-    </>
+      <CopyrightStatement />
+    </Grid>
   );
 };
 
